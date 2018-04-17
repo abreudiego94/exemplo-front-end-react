1869eceda3223032fab514ff4e43f2ed4680af6a
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2018-04-16 22:24:36 -0300

    Adicionando react-router no projeto

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Usuario } from './pages/Usuario';
 let axios = require('axios');
+import {
+  BrowserRouter, Route,
+  Link, Redirect,
+  withRouter,
+  Switch,
+} from 'react-router-dom';
+
 class App extends Component {
 
 
-
   render() {
-
-
     return (
       <Usuario />
 
